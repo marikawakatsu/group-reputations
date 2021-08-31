c998d555d31c0af095419e2d4c8efa87a6a04b5a
michel-mata <77646084+michel-mata@users.noreply.github.com>
2021-08-31 12:13:15 -0400

    renamed mechanism variables

--- module/structs.jl
+++ module/structs.jl
@@ -33,13 +33,13 @@ mutable struct Population
     all_groups::Array{Int64, 1}         # array of allowed groups
     group_sizes::Array{Int64, 1}        # array of group sizes
     # Individual reputations
-    ind_reps_type::Bool                 # individual reputation type: public or private
+    ind_reps_public::Bool                 # individual reputation type: public or private
     ind_reps_base::Array{Bool, 1}       # individuals reps are based on behavior (1) or not (0)
-    ind_reps_src::Array{Bool, 1}        # update individual reps based on individual (1) or group (0) reputation
+    ind_reps_src_ind::Array{Bool, 1}        # update individual reps based on individual (1) or group (0) reputation
     # Group reputations
-    grp_reps_type::Bool                 # group reputation type: public or private
+    grp_reps_public::Bool                 # group reputation type: public or private
     grp_reps_base::Array{Bool, 1}       # group reps are based on behavior (1) or not (0)
-    grp_reps_src::Array{Bool, 1}        # update group reps based on individual (1) or group (0) reputation
+    grp_reps_src_grp::Array{Bool, 1}        # update group reps based on individual (0) or group (1) reputation
     threshold::Float64                  # coupled with rates of updating
     # Storage
     strategies::Array{Int64, 1}         # array of strategies
