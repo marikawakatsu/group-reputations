c2b3841cd95d5d194d0881d20680c63cdc90abec
Mari Kawakatsu <kawakatsu.mari@gmail.com>
2021-09-02 23:32:52 -0400

    added a line plot for coop

--- rsrc/plotter.R
+++ rsrc/plotter.R
@@ -14,14 +14,16 @@ setwd("~/Documents/GitHub/group-reputations/")
 # LOAD SIMULATION DATA 
 ########################
 # path to the data directory
-data_dir  <- "~/Dropbox (Princeton)/Stereotypes_results/group-reputations/sweep_src-prob-rate/"
+data_dir     <- "~/Dropbox (Princeton)/Stereotypes_results/group-reputations/"
+data_sub_dir <- "sweep_src-prob-rate"
 
 # load and merge data
-simdata_m <- read.csv( paste0(data_dir, "m_data.csv"), header = TRUE)
-simdata_s <- read.csv( paste0(data_dir, "s_data.csv"), header = TRUE)
+simdata_m <- read.csv( paste0(data_dir, data_sub_dir, "/", "m_data.csv"), header = TRUE)
+simdata_s <- read.csv( paste0(data_dir, data_sub_dir, "/", "s_data.csv"), header = TRUE)
 simdata   <- rbind(simdata_m, simdata_s)
+# simdata_ss <- read.csv( paste0(data_dir,"sweep_base-src-prob", "/", "data.csv"), header = TRUE)
 
-casecount <- simdata %>% group_by(N, norm, ind_base, grp_base) %>% summarize(COUNT = n())
+casecount <- simdata %>% group_by(N, norm, ind_base, grp_base, ind_src_ind, grp_src_grp) %>% summarize(COUNT = n())
 
 ##########################
 # PREP DATA FOR PLOTTING
@@ -35,10 +37,10 @@ measure_rep_grp <- c("reps_grp_11","reps_grp_12","reps_grp_21","reps_grp_22")
 measure_freq    <- c("freq1_ALLC","freq1_ALLD","freq1_DISC","freq2_ALLC","freq2_ALLD","freq2_DISC")
 measure_fitness <- c("fitness1_ALLC","fitness1_ALLD","fitness1_DISC","fitness2_ALLC","fitness2_ALLD","fitness2_DISC")
 
-simdata$ind_base[simdata$ind_base == 0] <- "Individual reps random"
-simdata$ind_base[simdata$ind_base == 1] <- "Individual reps based on behavior"
-simdata$grp_base[simdata$grp_base == 0] <- "Group reps random"
-simdata$grp_base[simdata$grp_base == 1] <- "Group reps based on behavior"
+# simdata$ind_base_label[simdata$ind_base == 0] <- "Individual reps random"
+# simdata$ind_base_label[simdata$ind_base == 1] <- "Individual reps based on behavior"
+# simdata$grp_base_label[simdata$grp_base == 0] <- "Group reps random"
+# simdata$grp_base_label[simdata$grp_base == 1] <- "Group reps based on behavior"
 
 # select columns
 simdata_bymeasure <- simdata %>% 
@@ -48,15 +50,23 @@ simdata_bymeasure$value = as.numeric(simdata_bymeasure$value)
 
 # compute means and vars
 compute_mean_vars <- function( simdata_bymeasure, variables ){
-  simdata_bymeasure[which(simdata_bymeasure$variable %in% variables),] %>%
+  simdata_mean <- simdata_bymeasure[which(simdata_bymeasure$variable %in% variables),] %>%
     rename( Fraction = value, Metric = variable ) %>%
-    group_by( N, norm, ind_base, grp_base, prob, rate, Metric ) %>%
+    group_by( N, norm, ind_base, grp_base, ind_src_ind, grp_src_grp, prob, rate, Metric ) %>%
     summarise(
       Mean = mean(Fraction),
       SD = sd(Fraction),
       SE = sd(Fraction) / sqrt(length(Fraction)),
       numCases = length(Fraction)
     )
+  
+  simdata_mean$ind_base_label[simdata_mean$ind_base == 0] <- "Individual reps random"
+  simdata_mean$ind_base_label[simdata_mean$ind_base == 1] <- "Individual reps based on behavior"
+  simdata_mean$grp_base_label[simdata_mean$grp_base == 0] <- "Group reps random"
+  simdata_mean$grp_base_label[simdata_mean$grp_base == 1] <- "Group reps based on behavior"
+  simdata_mean <- simdata_mean %>% mutate( rate_label = paste0( "λ =", rate ))
+  
+  return(simdata_mean)
 }
 
 simdata_coop    <- compute_mean_vars(simdata_bymeasure, measure_coop)
@@ -65,10 +75,10 @@ simdata_rep_grp <- compute_mean_vars(simdata_bymeasure, measure_rep_grp)
 simdata_freq    <- compute_mean_vars(simdata_bymeasure, measure_freq)
 simdata_fitness <- compute_mean_vars(simdata_bymeasure, measure_fitness)
 
-#############
-# PLOT DATA
-#############
-plot_heatmap <- function(simdata_sub, metric = "cooperation", label = "Global\ncooperation", norm = "SJ"){
+################
+# PLOT HEATMAP
+################
+plot_heatmap <- function(simdata_sub, norm = "SJ", metric = "cooperation", label = "Average\ncooperation"){
   
   subdata <- simdata_sub[simdata_sub$Metric == metric & 
                          simdata_sub$norm == norm, ]
@@ -97,26 +107,81 @@ plot_heatmap <- function(simdata_sub, metric = "cooperation", label = "Global\nc
     labs(x = "Probability of DISC using individual reputations (p)",
          y = "Rate of group reputation update (λ)") +
     geom_tile( show.legend = TRUE ) + 
-    facet_grid(grp_base ~ ind_base, 
-               space="free", switch = "both", scales="free") +
+    facet_grid(grp_base_label ~ ind_base_label, 
+               space="free", scales="free") +
     theme(strip.placement = "outside",
-          strip.background = element_blank()) 
+          # strip.background = element_blank()
+          ) 
   
   return(fig)
 }
 
+################
+# PLOT LINES
+################
+plot_line_coop <- function(simdata_sub, norm = "SJ", metric = measure_coop[2:5], label = "Average\ncooperation"){
+  
+  # fix ind_base = 1
+  subdata <- simdata_sub[simdata_sub$Metric %in% metric & 
+                           simdata_sub$norm == norm & 
+                           simdata_sub$ind_base == TRUE &
+                           simdata_sub$rate %in% c(0,1), ]
+  
+  fig <- ggplot(data = subdata,
+                aes(x = prob, y = Mean, color = Metric, label = Metric)) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(plot.title = element_text(hjust = 0.5, 
+                                    size = 10, 
+                                    margin = margin(0,0,0,0) ) ) +
+    theme (legend.text = element_text (size = 7),
+           legend.title = element_text(size = 8),
+           legend.key.size = unit(0.04, "npc"),
+           panel.spacing = unit(0.25,"lines"),
+           legend.margin = margin(t = 0, unit="npc")
+    ) +
+    ggtitle( paste0("norm: ", norm) ) +
+    scale_x_continuous( breaks = seq(0, 1, 0.2)) +
+    scale_y_continuous( breaks = seq(0, 1, 0.2),
+                        limits = c(0, 1)) +
+    scale_color_manual(values = c("#2b8cbe","#2b8cbe","#a8ddb5","#a8ddb5"),
+                       name   = "Average\ncooperation") +
+    geom_errorbar(aes(ymin = Mean - SD, ymax = Mean + SD), width = 0, size = 0.3) +
+    geom_line(size = 0.4, alpha = 1, lty = 1) +
+    geom_point(stat="identity", size = 1.1, alpha = 1, stroke = 0.5) + #, shape = 21, fill = "white") +
+    labs(x = "Probability of DISC using individual reputations (p)",
+         y = "Frequency") +
+    facet_grid(grp_base_label ~ rate_label, 
+               space="free", scales="free") +
+    theme(strip.placement = "outside",
+          # strip.background = element_blank()
+          ) 
+  
+  return(fig)
+}
+
+##############
+# SAVE PLOTS
+##############
 # test plots
 simdata_sub <- simdata_coop
 metric      <- "cooperation"
-label       <- "Global\ncooperation"
+label       <- "Average\ncooperation"
 norms       <- unique(simdata$norm)
 
 for(norm in norms){
   
-  png(filename = paste0("plots/", metric, "_", norm, "_",
-                        format(Sys.Date(), format="%y%m%d"), "_SD.png"),
+  png(filename = paste0("plots/", metric, "_heatmap_", norm, "_",
+                        format(Sys.Date(), format="%y%m%d"), ".png"),
       width = 6, height = 5, units = "in", res = 600)
-  print(plot_heatmap(simdata_sub, metric, label, norm))
+  print(plot_heatmap(simdata_sub, norm, metric, label))
+  dev.off()
+  Sys.sleep(1)
+  
+  png(filename = paste0("plots/", metric, "_line_", norm, "_",
+                        format(Sys.Date(), format="%y%m%d"), ".png"),
+      width = 6, height = 5, units = "in", res = 600)
+  print(plot_line_coop(simdata_sub, norm))
   dev.off()
   Sys.sleep(1)
 }
